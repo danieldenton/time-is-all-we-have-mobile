bbe84e6dac0eb17960951f639afec3a54d2d34b7
Daniel Denton <danieldenton@Daniels-MacBook-Pro-2.local>
2023-10-09 17:48:58 -0700

    readme created

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>TIME IS ALL WE HAVE</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.text}>TIME IS ALL WE HAVE</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#20232a',
+    color: "#20232a",
     fontSize: 30,
-    fontWeight: 'bold'
-  }
+    fontWeight: "bold",
+  },
 });
