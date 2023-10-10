2161b4027737f96c579e94fd6c8e91caa53c1a7e
Daniel Denton <danieldenton@Daniels-MacBook-Pro-2.local>
2023-10-09 18:33:35 -0700

    account navoigator created

---
+++ src/navigation/AccountNavigator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "../HomeScreen";
+
+const AccountStack = createNativeStackNavigator();
+
+export const AccountNavigator = () => {
+  return (
+    <AccountStack.Navigator>
+      <AccountStack.Screen name="HomeScreen" component={HomeScreen} />
+    </AccountStack.Navigator>
+  );
+};
