c15a7eba2c7feaad06e5b0ec9e45c0ce5537dd64
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-10-11 17:47:14 -0700

    community navigation

--- src/screens/Community.tsx
+++ src/screens/Community.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { IStackScreenProps } from "../navigation/StackScreenProps";
 import { styles } from "../styles";
 
-export const CommunityScreen = () => {
+export const CommunityScreen: React.FunctionComponent<IStackScreenProps> = (
+  props
+) => {
+  const { navigation, route } = props;
   return (
     <View style={styles.container}>
       <Text style={styles.text}>COMMUNITY SCREEN</Text>
+      <Button
+        title="MEDITATE"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+      <Button
+        title="MEDITATE"
+        onPress={() => {
+          navigation.navigate("Meditation");
+        }}
+      />
+      <Button
+        title="MEDITATE"
+        onPress={() => {
+          navigation.navigate("About");
+        }}
+      />
+      <Button
+        title="MEDITATE"
+        onPress={() => {
+          navigation.navigate("Contact");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
