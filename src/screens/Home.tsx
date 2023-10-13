e2e836928a250fd3b876f73263ced0f59d6cf12c
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-10-12 17:31:48 -0700

    home nav updated

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Pressable } from "react-native";
 import { IStackScreenProps } from "../navigation/StackScreenProps";
-import { routes } from "../navigation/routes";
 import { styles } from "../styles";
 
 export const HomeScreen: React.FunctionComponent<IStackScreenProps> = (
@@ -10,26 +9,37 @@ export const HomeScreen: React.FunctionComponent<IStackScreenProps> = (
 ) => {
   const { navigation } = props;
 
-  const navNames = routes.filter((r) => r.name !== "Home");
-
-  const navButtons = navNames.map((r, i) => {
-    return (
-      <Pressable
-        key={i}
-        onPress={() => {
-          navigation.navigate(r.name);
-        }}
-      >
-        <Text>{r.name}</Text>
-      </Pressable>
-    );
-  });
-
-
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TIME IS ALL WE HAVE</Text>
-      {navButtons}
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Meditation");
+        }}
+      >
+        <Text>MEDITATION</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Community");
+        }}
+      >
+        <Text>COMMUNITY</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Contact");
+        }}
+      >
+        <Text>CONTACT</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("Contact");
+        }}
+      >
+        <Text>CONTACT</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
