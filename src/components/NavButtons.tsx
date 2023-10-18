78ec14950ebf6b24362f4291c8ad70515c1af7c5
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-10-18 15:52:08 -0700

    removed unnecessary imports

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Button } from "react-native";
-import { IStackScreenProps } from "../navigation/StackScreenProps";
-import { IRouteNames } from "../navigation/routeNames";
 
 export const NavButtons: React.FunctionComponent<any> = ({ navigation, names }) => {
 
