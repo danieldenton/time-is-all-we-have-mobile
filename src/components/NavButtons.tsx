ca56f04f825958f8c42fe1192aa4d84cc7c60d56
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-10-18 14:59:55 -0700

    navButtons not throwing errors. Navigation and names passed from home

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { Button } from "react-native";
-import { routes } from "../navigation/routes";
 import { IStackScreenProps } from "../navigation/StackScreenProps";
 
-export const NavButtons: React.FunctionComponent<IStackScreenProps> = (
-  props
-) => {
-  const { navigation } = props;
+export const NavButtons: React.FunctionComponent<IStackScreenProps & any> = ({ navigation, names }) => {
 
-  const navNames = routes.map((r, i) => {
-    return r.name;
-  });
-  const navButtons = navNames.map((name, i) => {
+  const navButtons = names.map((name: string, i: number) => {
     return (
       <Button
         key={i}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,45 +1,22 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, Pressable } from "react-native";
+import { Text, View } from "react-native";
 import { IStackScreenProps } from "../navigation/StackScreenProps";
 import { styles } from "../styles";
+import { NavButtons } from "../components/NavButtons";
+import { routes } from "../navigation/routes"
 
 export const HomeScreen: React.FunctionComponent<IStackScreenProps> = (
   props
 ) => {
   const { navigation } = props;
 
+const names = routes.map((r) => { return r.name })
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>TIME IS ALL WE HAVE</Text>
-      <Pressable
-        onPress={() => {
-          navigation.navigate("Meditation");
-        }}
-      >
-        <Text>MEDITATION</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          navigation.navigate("Community");
-        }}
-      >
-        <Text>COMMUNITY</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          navigation.navigate("Contact");
-        }}
-      >
-        <Text>CONTACT</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          navigation.navigate("About");
-        }}
-      >
-        <Text>ABOUT</Text>
-      </Pressable>
+      <NavButtons navigation={navigation} names={names} />
       <StatusBar style="auto" />
     </View>
   );
